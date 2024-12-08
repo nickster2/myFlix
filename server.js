28fe25213401cc6932cac21c3d7a9f4153cabf0e
nickster2 <gouldnick125@gmail.com>
2024-12-07 16:06:14 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const url = require("url"),
+  http = require("http");
+fs = require("fs");
+
+http
+  .createServer((request, response) => {
+    let addr = request.url;
+    q = new URL(addr, "http://" + request.headers.host);
+    filePath = "";
+
+    fs.appendFile(
+      "log.txt",
+      "URL: "
+      + addr + "/nTimestamp:" +
+      new Date() + "\n\n",
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log.");
+        }
+      }
+    );
+
+    if (q.pathname.includes("documentation")) {
+      filePath = __dirname + "/documentation.html";
+    } else {
+      filePath = "index.html";
+    }
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.write(data);
+      response.end("\nHello Node!\n");
+    });
+  })
+  .listen(8080);
+console.log("My first Node test server is runnning on Port 8080.");
